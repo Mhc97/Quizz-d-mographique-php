fe43f5b66f305b6465ab9146541589bf1918b128
Mouhcine <mouhcine@gmail.com>
2026-05-04 15:41:01 +0200

    commencer check_question

---
+++ quiz-api/api/check_question.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$input = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($input['questionId']) || !isset($input['selectionOption'])){
+    echo json_encode(["correct" => false, "error" => "Données manquantes"]);
+}
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dname=pays;charset=utd8','root','');
+    $stmt = $pdo->prepare('SELECR capital FROM pays WHERE id = ?');
+    $stmt->execute([$input['questionId']]);
+    $bonneReponse = $stmt->fetchColumn();
+
+    $correct = ($input['selectionOption'] === $bonneReponse);
+    echo json_encode(["correct" => $correct]);
+
+}catch (PDOException $e){
+    echo json_encode(["correct" => false, "error" => $e->getMessage()]);
+}
+?>
--- quiz-api/api/get_question.php
+++ quiz-api/api/get_question.php
@@ -5,5 +5,6 @@ header("Content-Type: application/json");
 // Appel à l'API Shadify pour une question sur les capitales
 $apiUrl = "https://shadify.yurace.pro/api/countries/capital-quiz?variants=4&amount=1";
 $reponse = file_get_contents($apiUrl);
+
 echo $reponse;
 ?>
