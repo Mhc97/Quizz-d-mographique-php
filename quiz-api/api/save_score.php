f85a88aeeb4eefc8e037ee06bffcf11d6b790cf4
Mouhcine <mouhcine@gmail.com>
2026-05-06 17:49:19 +0200

    j'ai fais save_score

--- quiz-api/api/save_score.php
+++ quiz-api/api/save_score.php
@@ -6,22 +6,25 @@ require_once '../config/database.php';
 
 $input = json_decode(file_get_contents("php//input"), true);
 
-if (!isset($input['questionId']) || !isset($input['selectedOption'])){
+if (!isset($input['pseudo']) || !isset($input['mode'])){
     echo json_encode(["correct" => false, "error" => "Données manquantes"]);
     exit;
 }
 
+$pseudo = htmlspecialchars($input['pseudo'], ENT_QUOTES, 'UTF-8');
+$score = intval($input['score']);
+$pseudo = htmlspecialchars($input['mode'], ENT_QUOTES, 'UTF-8');
 try{
     $database = new Database();
     $pdo = $database->getConnection();
 
-    $stmt = $pdo->prepare("SELECT capital FROM pays WHERE id = ?");
-    $stmt->execute([$input['questionId']]);
-    $bonneReponse = $stmt->fetchColumn();
+    $sql = "INSERT INTO scores (pseudo, score, mode) VALUES (:psuedo, :score, :mode)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['pseudo' => $pseudo, 'score' => $score, 'mode' => $mode]);
 
-    $correct = ($input['selectedOption'] === $bonneReponse);
-    echo json_encode(["correct" => $correct]);
+
+    echo json_encode(["success" => true]);
 }catch (Exception $e){
-    echo json_encode(["correct" => false, "error" => $e->getMessage()]);
+    echo json_encode(["success" => false, "error" => $e->getMessage()]);
 }
 ?>
