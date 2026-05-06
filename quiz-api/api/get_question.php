397d34d133f5d4a31ba1f0b212ce53fcb0c82ac9
Mouhcine <mouhcine@gmail.com>
2026-05-06 17:26:38 +0200

    changer la partie get_questions

--- quiz-api/api/get_question.php
+++ quiz-api/api/get_question.php
@@ -1,10 +1,48 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+ini_set('diplay_errors', 1);
+error_reporting(E_ALL);
+
+require_once '../config/database.php';
 
 // Appel à l'API Shadify pour une question sur les capitales
-$apiUrl = "https://shadify.yurace.pro/api/countries/capital-quiz?variants=4&amount=1";
-$reponse = file_get_contents($apiUrl);
+// $apiUrl = "https://shadify.yurace.pro/api/countries/capital-quiz?variants=4&amount=1";
+// $reponse = file_get_contents($apiUrl);
 
-echo $reponse;
+// echo $reponse;
+try{
+    $database = new database();
+    $pdo = $database->getConnection();
+
+    // pour choisr un pays au hasard 
+    $stmt = $pdo->query("SELECT id, name, capital FROM pays ORDER BY RAND() LIMIT 1");
+    $pays = $stmt->fetch(PDO::FETCH_ASSOC).
+    $pays = throw new Exception("Aucun pays trouvé");
+
+    $bonneReponse = $pays['capital'];
+    $paysId = $pays['id'];
+    $nomPays = $pays['name'];
+
+    // Récupérer 3 autres capitales aléatoires
+    $stmtAutres = $pdo->prepare("SELECT capital FROM pays WHERE id != :id ORDER BY RAND() LIMIT 3");
+    $stmtAutres->execute(['id' => $paysId]);
+    $autresCapitales = $stmtAutres->fetchAll(PDO::FETCH_COLUMN);
+
+    // Construire les options
+    $options = array_merge([$bonneReponse], $autresCapitales);
+    shuffle($options);
+
+    // Retourner la question
+    $data = [
+        "id" => $paysId,
+        "texte" => "Quelle est la capitale de $nomPays ?",
+        "oprions" => $options,
+        "bonne_reponse" => $bonneReponse
+    ];
+
+    echo json_encode($data);
+}catch (Exception $e){
+    echo json_encode(["error" => $e->getMessage()]);
+}
 ?>
