e16d913a4afbf670478b8544f74d558f1010abf5
Mouhcine <mouhcine@gmail.com>
2026-05-08 17:30:58 +0200

    remplir le dossier api.jsx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,23 @@
 import axios from 'axios'
 
-const API_base = 'http://localhost/Quiz_démographique/backend/api';
+const API_BASE = 'http://localhost/Quiz_démographique/backend/api';
+
+export const getQuestion = async () => {
+    const res = await axios.get(`${API_BASE}/get_question.php`):
+    return res.data;
+};
+
+export const chekAnswer = async (questionId, selectedOption) => {
+    const res = await axios.post(`${API_BASE}/check_answer.php, {questionId, selectedOption}`);
+    return res.data.correct;
+};
+
+export const saveScore = async (pseudo, ScrollRestoration, mode)=>{
+    const res = await axios.post(`${API_BASE}/save_score.php`, {pseudo, score, mode});
+    return res.data.success;
+};
+
+export const getScores = async () => {
+    const res = await axios.get(`${API_BASE}/get_scores.php`);
+    return res.data;
+};
