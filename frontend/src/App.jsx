b65f96f0b0bde2f8fd7334a8eb8d1c558cdda520
Mouhcine <mouhcine@gmail.com>
2026-05-08 17:41:17 +0200

    remplis le fichier App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Accueil from './pages/Acceuil';
+import Quiz from './pages/Quiz';
+import Resultats from './pages/Resultats';
+
 
 function App() {
 
   return (
     <>
-      
+      return(
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Acceuil/>}/>
+          <Route path="/quiz" element={<Quiz/>}/>
+          <Route path="/resultats" element={<Resultats/>}/>
+        </Routes>
+        </BrowserRouter>
+      );
+
     </>
   )
 }
 
-export default App
+export default App;
