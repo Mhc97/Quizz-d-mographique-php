94cc84f1a3803b9d4b1439e2fd8859dbda8ca039
Mouhcine <mouhcine@gmail.com>
2026-05-09 19:47:06 +0200

    ajouter Navbar et Footer dans le fichier App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,26 @@ import './App.css';
 import Accueil from './pages/Accueil';
 import Quiz from './pages/Quiz';
 import Resultats from './pages/Resultats';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 
 function App() {
 
   return (
     <>
+   
         <BrowserRouter>
+         <Navbar/>
         <Routes>
           <Route path="/" element={<Accueil/>}/>
           <Route path="/quiz" element={<Quiz/>}/>
           <Route path="/resultats" element={<Resultats/>}/>
         </Routes>
+        <Footer/>
         </BrowserRouter>
 
+    
     </>
   )
 }
