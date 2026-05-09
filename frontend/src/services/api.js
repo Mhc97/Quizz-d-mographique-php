a90a9105c22790c02322a414b71712280edc42e2
Mouhcine <mouhcine@gmail.com>
2026-05-09 15:53:07 +0200

    debogage pour faire fonctionner le quizz en allant sur api.js et check_answer.php

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,30 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const API_BASE = 'http://localhost/Quiz_démographique/backend/api';
+// const API_BASE = 'http://localhost/Quizz%20d%C3%A9mographique/back-end/quiz-api/api';
+
+// export const getQuestion = async () => {
+//     const res = await axios.get(`${API_BASE}/get_question.php`);
+//     return res.data;
+// };
+
+// export const checkAnswer = async (questionId, selectedOption) => {
+//     const res = await axios.post(`${API_BASE}/check_answer.php,`,{questionId, selectedOption});
+//     return res.data.correct;
+// };
+
+// export const saveScore = async (pseudo, score, mode)=>{
+//     const res = await axios.post(`${API_BASE}/save_score.php`, {pseudo, score, mode});
+//     return res.data.success;
+// };
+
+// export const getScores = async () => {
+//     const res = await axios.get(`${API_BASE}/get_scores.php`);
+//     return res.data;
+// };
+
+import axios from 'axios';
+
+const API_BASE = 'http://localhost/Quizz%20d%C3%A9mographique/back-end/quiz-api/api';
 
 export const getQuestion = async () => {
     const res = await axios.get(`${API_BASE}/get_question.php`);
@@ -8,11 +32,11 @@ export const getQuestion = async () => {
 };
 
 export const checkAnswer = async (questionId, selectedOption) => {
-    const res = await axios.post(`${API_BASE}/check_answer.php, `,{questionId, selectedOption});
+    const res = await axios.post(`${API_BASE}/check_answer.php`, {questionId, selectedOption});
     return res.data.correct;
 };
 
-export const saveScore = async (pseudo, score, mode)=>{
+export const saveScore = async (pseudo, score, mode) => {
     const res = await axios.post(`${API_BASE}/save_score.php`, {pseudo, score, mode});
     return res.data.success;
 };
