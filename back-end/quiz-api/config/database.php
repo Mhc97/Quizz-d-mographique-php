546c522ed17a06da300f5c64c3f6f063c739b2e9
Mouhcine <mouhcine@gmail.com>
2026-05-09 15:12:57 +0200

    fix(config): modification de database.php pour le support JSON

--- back-end/quiz-api/config/database.php
+++ back-end/quiz-api/config/database.php
@@ -1,15 +1,18 @@
 <?php
 class database {
     private $host = "localhost";
-    private $dbname = "pays";
-    private $username = "";
+    private $dbname = "coursmysql";
+    private $username = "root";
     private $password = "";
     private $conn;
 
     public function getConnection(){
         $this->conn = null;
         try{
-            $this->conn = new PDO("mysql:host". $this->host . ";dbname" .$this->dbname, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", 
+            $this->username, 
+            $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
         }catch(PDOException $e){
