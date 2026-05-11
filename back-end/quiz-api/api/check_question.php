e9244b3dd7e4a94fc29dac51bbe5ad934224f099
Mouhcine <mouhcine@gmail.com>
2026-05-11 07:24:19 +0200

    feat(quizz): suppression de fichier inutile Quiz.php

--- back-end/quiz-api/api/check_question.php
+++ back-end/quiz-api/api/check_question.php
@@ -4,13 +4,13 @@ header("Content-Type: application/json");
 
 $input = json_decode(file_get_contents("php://input"), true);
 
-if (!isset($input['questionId']) || !isset($input['selectionOption'])){
+if (!isset($input['questionId']) || !isset($input['selectedOption'])){
     echo json_encode(["correct" => false, "error" => "Données manquantes"]);
 }
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dname=pays;charset=utd8','root','');
-    $stmt = $pdo->prepare('SELECR capital FROM pays WHERE id = ?');
+    $pdo = new PDO('mysql:host=localhost;dname=pays;charset=utf8','root','');
+    $stmt = $pdo->prepare('SELECT capital FROM pays WHERE id = ?');
     $stmt->execute([$input['questionId']]);
     $bonneReponse = $stmt->fetchColumn();
 
